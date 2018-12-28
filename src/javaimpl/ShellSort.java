f984d5bcf4d818de71ccb1a703c5472693d38ab0
Bill <ywbandroid@gmail.com>
2018-12-28 14:15:24 +0800

    希尔排序

--- src/javaimpl/ShellSort.java
+++ src/javaimpl/ShellSort.java
@@ -11,16 +11,16 @@ public class ShellSort implements ISort {
     @Override
     public int[] sort(int[] data) {
         int[] newData = newArray(data);
-        for (int step = newData.length / 2; step > 0; step /= 2) {
-            for (int rightShift = step; rightShift < newData.length; rightShift++) {
-                int temp = newData[rightShift];
+        int length = newData.length;
+        for (int step = length / 2; step > 0; step /= 2) {
+            for (int rightShift = step; rightShift < length; rightShift++) {
+                int compareData = newData[rightShift];
                 int leftShift = rightShift;
-
-                while (leftShift - step >= 0 && newData[leftShift - step] > temp) {
+                while (leftShift - step >= 0 && newData[leftShift - step] > compareData) {
                     newData[leftShift] = newData[leftShift - step];
                     leftShift = leftShift - step;
                 }
-                newData[leftShift] = temp;
+                newData[leftShift] = compareData;
             }
         }
         return newData;
