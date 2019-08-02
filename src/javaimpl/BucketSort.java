4d07f4e25dbe69327bece6481d068f65a04e3d6f
Bill <ywbandroid@gmail.com>
2019-08-02 19:04:00 +0800

    .

--- src/javaimpl/BucketSort.java
+++ src/javaimpl/BucketSort.java
@@ -3,7 +3,6 @@ package javaimpl;
 import inter.ISort;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 /**
@@ -18,25 +17,29 @@ public class BucketSort implements ISort {
     }
 
     private void bucketSort(int[] data) {
+        // 确认桶的大小
         int length = data.length;
         int max = getMaxAndMinData(data)[0];
         int min = getMaxAndMinData(data)[1];
         int bucketNum = (max - min) / length + 1;
 
+        // 创建bucketNum个桶
         List<List<Integer>> buckets = new ArrayList<>(bucketNum);
         for (int i = 0; i < bucketNum; i++) {
             buckets.add(new ArrayList<>());
         }
 
+        // 将数组中的数据分别放到对应的桶中
         for (int i = 0; i < data.length; i++) {
             int bucketIndex = (data[i] - min) / length;
             buckets.get(bucketIndex).add(data[i]);
         }
 
         for (int i = 0; i < buckets.size(); i++) {
-            Collections.sort(buckets.get(i));
+            insertSort(buckets.get(i), buckets.get(i).size());
         }
 
+        // 依次将桶中的数据放回到原数组中
         for (int i = 0, k = 0; i < buckets.size(); i++) {
             List<Integer> bucket = buckets.get(i);
             for (int j = 0; j < bucket.size(); j++) {
@@ -66,6 +69,19 @@ public class BucketSort implements ISort {
         }
 
         return arr;
+    }
+
+    // 插入排序
+    private void insertSort(List<Integer> data, int n) {
+        for (int rightShift = 1; rightShift < n; rightShift++) {
+            int compareData = data.get(rightShift);
+            int leftShift = rightShift - 1;
+            while (leftShift >= 0 && data.get(leftShift) > compareData) {
+                data.set(leftShift + 1, data.get(leftShift));
+                leftShift--;
+            }
+            data.set(leftShift + 1, compareData);
+        }
     }
 
 }
--- src/javaimpl/CountSort.java
+++ src/javaimpl/CountSort.java
@@ -14,15 +14,18 @@ public class CountSort implements ISort {
     }
 
     private void countSort(int[] data) {
+        // 确定桶大小
         int length = data.length;
         int max = getMaxAndMinData(data)[0];
         int min = getMaxAndMinData(data)[1];
         int[] buckets = new int[max - min + 1];
 
+        // 每个桶中放的不是数组中的数，而是数组中数的个数
         for (int i = 0; i < length; i++) {
             buckets[data[i] - min]++;
         }
 
+        // 对buckets数组顺序求和，则buckets的含义为：buckets[i]里存的是小于等于i的数的个数
         for (int i = 1; i < buckets.length; i++) {
             buckets[i] += buckets[i - 1];
         }
@@ -30,6 +33,7 @@ public class CountSort implements ISort {
         int[] tempArr = new int[length];
 
         // 倒序遍历，下面注释的正序遍历，正序遍历就变成不稳定排序了
+        // 根据buckets中每个数的含义可以确定所在的下标，取出一个少一个，对应下标-1
         for (int i = length - 1; i >= 0; i--) {
             int value = data[i] - min;
             tempArr[buckets[value] - 1] = data[i];
